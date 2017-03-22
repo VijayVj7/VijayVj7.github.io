4878fa8a2005731e911319a1fda751ef9a145d02
VijayVj7 <vijaycsemvgr@gmail.com>
2017-03-22 23:07:08 +0530

    Jquery File is added as part of the project
    It is a great leap for us to move forward and build better

---
+++ script.js
@@ -0,0 +1,15 @@
+ 
+ $(document).ready(function(){
+       
+     $('.btn-outline').hover(function(){
+          $(this).css('background-color','#fff');
+     },
+      function(){
+          $(this).css('background-color','transparent');
+          //$(this).css('transition','background-color 0.5s ease-in-out');
+      
+      }
+      );
+
+ }); 
+ 
